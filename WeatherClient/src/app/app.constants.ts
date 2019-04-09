f4d1bbf5ead934aef00385e22a68999cb71432d2
Ferrir <ferreira.alexandre@gmail.com>
2019-04-09 16:38:34 -0300

    primeiro commit

---
+++ WeatherClient/src/app/app.constants.ts
@@ -0,0 +1,6 @@
+export class Constants {
+  static locationAPIUrl = 'http://dataservice.accuweather.com/locations/v1';
+  static currentConditionsAPIUrl = 'http://dataservice.accuweather.com/currentconditions/v1';
+  /* please register https://developer.accuweather.com/ to get APIKey */
+  static apiKey = 'O6i2l9sqfbXAGXmSKqtwUuEcYGU4LHJH';
+}
